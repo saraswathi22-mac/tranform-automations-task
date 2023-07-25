ac1390987f46190f21ecebb8a634f538de2c2308
exit <saraswathi.mac@gmail.com>
2023-07-25 20:18:48 +0530

    Updated React router DOM

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import Main from "./components/Main/Main";
 import Resource from "./components/Resource/Resource";
@@ -9,11 +9,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route exact path="/resources" component={Main} />
-          <Route exact path="/resource/:id" component={Resource} />
-          <Route component={NotFound} />
-        </Switch>
+        <Routes>
+          <Route path="/resources" element={<Main />} />
+          <Route path="/resource/:id" element={<Resource />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </Router>
   );
