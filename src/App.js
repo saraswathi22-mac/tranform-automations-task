29dd875c324fd57214abac3b7bbdefaa4ac64fff
exit <saraswathi.mac@gmail.com>
2023-07-25 20:13:28 +0530

    Updated the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import Main from "./components/Main/Main";
-import Resource from './components/Resource/Resource';
+import Resource from "./components/Resource/Resource";
+import React from "react";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>  
-          <Route exact path="/resources" component={Main}/>
+        <Switch>
+          <Route exact path="/resources" component={Main} />
           <Route exact path="/resource/:id" component={Resource} />
           <Route component={NotFound} />
         </Switch>
